5c84f28311f781971b2979f0c3e8bb3d90a6b611
Jonathan Sawyer <jmsawyer@alaska.edu>
2013-01-23 11:13:02 -0900

    Adds geomag.js test script.

---
+++ geomag.js
@@ -0,0 +1,13 @@
+/**
+ * File: client.js
+ * Author: Jonathan Sawyer
+ * Copyright: 2012, Poker Flat Research Range, University of Alaska Fairbanks
+ * License: MIT License
+ */
+
+// Requires
+var geoMag = require('./lib/geomagJS/geomag.js');
+
+var gm = geoMag(65, -147, /* optional, in feet */ 5280, /* optional */ new Date());
+
+console.log(gm);
